e1aa9b72067480b8b7ed76a916b76367d766985e
AmosMaru <amosmaru10@gmail.com>
2024-06-17 00:27:54 +0300

    app.py

--- app.py
+++ app.py
@@ -124,4 +124,4 @@ def disease_prediction():
     return jsonify({"error": "Invalid request method"})
 # ===============================================================================================
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000, debug=True)
+    app.run(host='0.0.0.0', port=6010, debug=True)
